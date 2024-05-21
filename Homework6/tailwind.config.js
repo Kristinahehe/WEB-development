2fdd656cd736141b1ada46255be8ae59207961a5
Kristina <kristina.krizanovska0@gmail.com>
2024-05-21 16:35:33 +0200

    Homerowk6.tailwind.config

---
+++ Homework6/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        kitty:
+          "url('/images/Homework6/Images/iStock-1313021528-min-1024x683.jpg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
+  },
+  plugins: [],
+};
