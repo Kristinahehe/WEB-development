8ad129d43bc9a71875d184e7743c0a93d52c358a
Kristina <kristina.krizanovska0@gmail.com>
2024-05-30 18:29:43 +0200

    HOMEWORK8

--- Homework8/app/index.php
+++ Homework8/app/index.php
@@ -14,7 +14,7 @@
       <div class="w-full max-w-3xl mx-auto p-8">
           <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md border dark:border-gray-700">
               <h1 class="text-2xl font-bold text-gray-800 dark:text-white mb-4">Personal information</h1>
-  
+  <?php echo var_dump($_POST);?>
               <div class="mb-6">
                   <h2 class="text-gray-700 dark:text-white mb-2">Use a permanent address where you can receive mail.</h2>
                   </div>
@@ -22,21 +22,21 @@
                   <div class="grid grid-cols-2 gap-4">
                       <div>
                           <label for="first_name" class="block text-gray-700 dark:text-white mb-1">First Name</label>
-                          <input type="text" id="first_name" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                          <input type="text" name="first_name" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                       </div>
                       <div>
-                          <label for "last_name" class="block text-gray-700 dark:text-white mb-1">Last Name</label>
-                          <input type="text" id="last_name" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                          <label for="last_name" class="block text-gray-700 dark:text-white mb-1">Last Name</label>
+                          <input type="text" name="last_name" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                       </div>
                   </div>
   
                   <div class="mt-4">
-                      <label for="address" class="block text-gray-700 dark:text-white mb-1">Email address</label>
-                      <input type="text" id="address" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                      <label for="email_address" class="block text-gray-700 dark:text-white mb-1">Email address</label>
+                      <input type="text" name="email_address" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                   </div>
   
                   <label for="countries" class="block text-gray-700 dark:text-white mb-1 text-sm font-medium ">Country</label>
-                      <select id="countries" class="text-sm">
+                      <select name="countries" class="text-sm">
                       <option>-</option>
                       <option>United States</option>
                       <option>Canada</option>
@@ -45,44 +45,45 @@
                   </select>
 
                   <div>
-                    <label for="address" class="block text-gray-700 dark:text-white mb-1">Street address</label>
-                    <input type="text" id="state" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                    <label for="street" class="block text-gray-700 dark:text-white mb-1">Street address</label>
+                    <input type="text" name="street" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                 </div>
 
   
                   <div class="grid grid-cols-3 gap-4 mt-4">
                       <div>
                           <label for="city" class="block text-gray-700 dark:text-white mb-1">City</label>
-                          <input type="text" id="state" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                          <input type="text" name="city" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                       </div>
                       <div>
                           <label for="state" class="block text-gray-700 dark:text-white mb-1">State/Province</label>
-                          <input type="text" id="zip" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                          <input type="text" name="state" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                       </div>
                       <div>
                         <label for="zip" class="block text-gray-700 dark:text-white mb-1">Zip/Postal code</label>
-                        <input type="text" id="zip" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
+                        <input type="text" name="zip" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none">
                     </div>
                   </div>
               </div>
+              <input type="submit">
               </form>
               <div>
                 
               <?php 
 
-                $First_name = $_POST['First_name'];
+                $first_name = $_POST['first_name'];
                 $last_name = $_POST['last_name'];
-                $email = $_POST['email'];
+                $email_address = $_POST['email_address'];
                 $country = $_POST['countries'];
-                $address = $_POST['address'];
+                $street = $_POST['street'];
                 $city = $_POST['city'];
                 $state = $_POST['state'];
                 $zip = $_POST['zip'];
 
-            if (!empty($name) and !empty($last_name) and !empty($email) and !empty($country) and !empty($street) and !empty($city) and !empty($state) and !empty($zip)) {
-                echo "<p>First_name: " .$Firstname. "</p>";
-                echo "<p>Last_name: " .$lastname. "</p>";
-                echo "<p>Email: " .$email. "</p>";
+            if (!empty($first_name) and !empty($last_name) and !empty($address) and !empty($countries) and !empty($street) and !empty($city) and !empty($state) and !empty($zip)) {
+                echo "<p>first_name: " .$first_name. "</p>";
+                echo "<p>Last_name: " .$last_name. "</p>";
+                echo "<p>email_address: " .$email_address. "</p>";
                 echo "<p>Countries: " .$countries. "</p>";
                 echo "<p>Adress: " .$address. "</p>";
                 echo "<p>City: " .$city. "</p>";
